a0847934e9df4fc914a8f722a058bb2606e95f89
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-21 17:19:51 -0400

    Update prototype.creep.js
    simplify code

--- prototype.creep.js
+++ prototype.creep.js
@@ -1,17 +1,10 @@
 module.exports = function() {
-	Creep.prototype.moveToEnergy =
-	function(target) {
-		if (this.pos.isNearTo(target)) {
-			this.withdraw(target, RESOURCE_ENERGY)
-		} else {
-			this.travelTo(target, {ignoreCreeps: false});
-		}
-	}
 	Creep.prototype.getEnergy =
 	function() {
 		var resByType = Game.rooms[this.room.name].resByType;
 		var dropedEnergy = resByType[RESOURCE_ENERGY];
 		var structByType = Game.rooms[this.room.name].structByType;
+		var spawns = structByType[STRUCTURE_SPAWN] || [];
 		var containers = structByType[STRUCTURE_CONTAINER] || [];
 		var storage = structByType[STRUCTURE_STORAGE] || [];
 		var energyStorage = containers.concat(storage);
@@ -23,7 +16,11 @@ module.exports = function() {
 			var targets = _.filter(energyStorage, (s) => s.store[RESOURCE_ENERGY] >= Math.min(200, this.store.getFreeCapacity(RESOURCE_ENERGY)));
 		}
 		var target = this.pos.findClosestByPath(targets);
-		this.moveToEnergy(target);
+		if (this.pos.isNearTo(target)) {
+			this.withdraw(target, RESOURCE_ENERGY)
+		} else {
+			this.travelTo(target, {ignoreCreeps: false});
+		}
 	}
 	Creep.prototype.mine =
 	function() {
