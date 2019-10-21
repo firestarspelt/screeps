226539d90b8675f1ffcffd95c0e086168e8f2c4b
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-21 15:08:45 -0400

    Update prototype.creep.js

--- prototype.creep.js
+++ prototype.creep.js
@@ -10,17 +10,21 @@ module.exports = function() {
 	Creep.prototype.getEnergy =
 	function() {
 		var resByType = Game.rooms[this.room.name].resByType;
-		var dropedEnergy = this.pos.findClosestByPath(resByType[RESOURCE_ENERGY]);
+		var dropedEnergy = resByType[RESOURCE_ENERGY];
+		var structByType = Game.rooms[this.room.name].structByType;
+		var containers = structByType[STRUCTURE_CONTAINER] || [];
+		var storage = structByType[STRUCTURE_STORAGE] || [];
+		var spawns = structByType[STRUCTURE_SPAWN] || [];
+		var energyStorage = containers.concat(storage);
 		if (dropedEnergy) {
 			console.log(this.name+' test');
-			this.moveToEnergy(dropedEnergy);
-		} else {
-			var structByType = Game.rooms[this.room.name].structByType;
-			var containers = structByType[STRUCTURE_CONTAINER] || [];
-			var storage = structByType[STRUCTURE_STORAGE] || [];
-			var targets = _.filter((containers.concat(storage)), (s) => s.store[RESOURCE_ENERGY] >= Math.min(200, this.getFreeCapacity(RESOURCE_ENERGY)));
-			var target = this.pos.findClosestByPath(targets);
-			this.moveToEnergy(target);
+			var targets = dropedEnergy;
+		} else if (energyStorage.length == 0) {
+			var targets = _.filter(spawns, (s) => s.store[RESOURCE_ENERGY] >= Math.min(200, this.store.getFreeCapacity(RESOURCE_ENERGY)));
+		} else if (!targets) {
+			var targets = _.filter(energyStorage, (s) => s.store[RESOURCE_ENERGY] >= Math.min(200, this.store.getFreeCapacity(RESOURCE_ENERGY)));
 		}
+		var target = this.pos.findClosestByPath(targets);
+		this.moveToEnergy(target);
 	}
 };
