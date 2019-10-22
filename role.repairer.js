fa1bb19cf119f2159ad83d151a5fce228f6e8e0a
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-22 00:13:17 -0400

    Update role.repairer.js
    fix repairer repairing along path

--- role.repairer.js
+++ role.repairer.js
@@ -11,14 +11,9 @@ module.exports = {
 			creep.memory.working = true;
 			creep.say('🛠️ repair');
 		}
-		var structures = creep.room.lookForAtArea(LOOK_STRUCTURES,creep.pos.y - 2,creep.pos.x - 2,creep.pos.y + 2,creep.pos.x + 2, true);
+		var structures = creep.pos.findInRange(Game.rooms[creep.room.name].infrastructure,1);
 		if (structures.length > 0) {
-			for (let structure of structures) {
-				if (structure.hits < structure.hitsMax - creep.memory.workParts * 100) {
-					creep.repair(structure);
-					break;
-				}
-			}
+			creep.repair(structures[0]);
 		}
 		if (creep.memory.working) {
 			var walls = Game.rooms[creep.room.name].walls;
