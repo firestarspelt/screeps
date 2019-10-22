c9c9c833f69f2f3e3ada7606c09a85a21164badf
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-21 22:38:04 -0400

    Update role.repairer.js

--- role.repairer.js
+++ role.repairer.js
@@ -32,7 +32,7 @@ module.exports = {
 				if (creep.repair(target) == ERR_NOT_IN_RANGE) {
 					creep.travelTo(target,{ignoreCreeps: false, range: 3});
 				}
-				if (target.hits / (target.hitsMax - creep.memory.workParts * 100) <= 1) {
+				if (target.hits / (target.hitsMax - creep.memory.workParts * 100) >= 1) {
 					delete creep.memory.target;
 				}
 			} else if (creep.repair(target) == ERR_INVALID_TARGET && creep.room.controller.level > 1) {
