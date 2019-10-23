889f003df06ec7160b70528e0fe2929deb7ac754
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-23 00:08:03 -0400

    Update role.supplier.js
    improvement to supplier AI to prioritize targets and cache it's target

--- mycode/role.supplier.js
+++ mycode/role.supplier.js
@@ -11,14 +11,42 @@ module.exports = {
 	    }
 		var structByType = Game.rooms[creep.room.name].structByType;
 		if (creep.memory.working) {
-			var spawns = structByType[STRUCTURE_SPAWN] || [];
-			var extensions = structByType[STRUCTURE_EXTENSION] || [];
-			var towers = structByType[STRUCTURE_TOWER] || [];
-			var storage = structByType[STRUCTURE_STORAGE] || [];
-			var targets = _.filter(spawns.concat(extensions).concat(towers).concat(storage), (s) => (s.store.getFreeCapacity(RESOURCE_ENERGY) > 0));
-			var target = creep.pos.findClosestByRange(targets);
-			if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-				creep.travelTo(target, {ignoreCreeps: false});
+			++creep.memory.timeSinceLastCheck;
+			//get target and put in memory if it doesn't exist
+			if (!creep.memory.target && creep.memory.timeSinceLastCheck > 10) {
+				let targets = (structByType[STRUCTURE_SPAWN] && structByType[STRUCTURE_EXTENSION] && structByType[STRUCTURE_TOWER] && structByType[STRUCTURE_STORAGE]);
+				let filteredTargets = _.filter(targets, (s) => (s.store.getFreeCapacity(RESOURCE_ENERGY) > 0));
+				let targetsByType = _.groupBy(filteredTargets, (s) => s.structureType);
+				let spawns = targetsByType[STRUCTURE_SPAWN] || [];
+				let extensions = targetsByType[STRUCTURE_EXTENSION] || [];
+				let towers = targetsByType[STRUCTURE_TOWER] || [];
+				let storage = targetsByType[STRUCTURE_STORAGE] || [];
+				if (spawns.length > 0) {
+					var target = creep.pos.findClosestByRange(spawns);
+				} else if (extensions.length > 0) {
+					var target = creep.pos.findClosestByRange(extensions);
+				} else if (towers.length > 0) {
+					var target = creep.pos.findClosestByRange(towers);
+				} else if (storage.length > 0) {
+					var target = creep.pos.findClosestByRange(storage);
+				}
+				if (target) {
+					creep.memory.target = target.id;
+				}
+				creep.memory.timeSinceLastCheck = 0;
+			}//get target from memory
+			else if (creep.memory.target) {
+				let target = Game.getObjectById(creep.memory.target);
+				//if target is full purge from memory
+				if (target.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
+					delete creep.memory.target;
+				}
+				//if target is still in memory, and in range tranfser to it, if it isn't move to it
+				if (creep.memory.target) {
+					if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+						creep.travelTo(target);
+					}
+				}
 			}
 		} else {
 			var containers = structByType[STRUCTURE_CONTAINER] || [];
