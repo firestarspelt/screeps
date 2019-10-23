a7cd1739ce4c4b69394776923de27c302de6246c
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-23 16:18:48 -0400

    Update role.room.js
    organize and add new room vars

--- mycode/role.room.js
+++ mycode/role.room.js
@@ -17,18 +17,28 @@ const roleRoom = {
 			var droppedEnergy = resByType[RESOURCE_ENERGY] || [];
 		}
 		const hitsPercentage = (s) => (s.hits/s.hitsMax);
+		//construction site variables
 		Game.rooms[room.name].constuctSites = room.find(FIND_CONSTRUCTION_SITES);
 		Game.rooms[room.name].constuctByType = _.groupBy(Game.rooms[room.name].constuctSites, (s) => s.structureType);
+
+		//structure variables
 		Game.rooms[room.name].structures = room.find(FIND_STRUCTURES);
 		Game.rooms[room.name].damStructures = _.filter(Game.rooms[room.name].structures, (s) => hitsPercentage < 0.8);
 		Game.rooms[room.name].structByType = _.groupBy(Game.rooms[room.name].structures, (s) => s.structureType);
+		[Game.rooms[room.name].walls, Game.rooms[room.name].infrastructure] = _.partition(Game.rooms[room.name].damStructures, (s) => s.structureType == STRUCTURE_WALL || s.structureType == STRUCTURE_RAMPART);
+		Game.rooms[room.name].walls = _.sortBy(Game.rooms[room.name].walls, hitsPercentage);
+		Game.rooms[room.name].infrastructure = _.sortBy(Game.rooms[room.name].infrastructure, hitsPercentage);
+
+		//resource variables
+		Game.rooms[room.name].sources = room.find(FIND_SOURCES);
 		Game.rooms[room.name].ruins = room.find(FIND_RUINS, {filter: (r) => r.store.getUsedCapacity() > 0});
 		Game.rooms[room.name].tombstones = room.find(FIND_TOMBSTONES, {filter: (t) => t.store.getUsedCapacity() > 0});
 		Game.rooms[room.name].droppedRes = room.find(FIND_DROPPED_RESOURCES, {filter: (r) => r.amount > 100});
 		Game.rooms[room.name].resByType = _.groupBy(Game.rooms[room.name].droppedRes, (r) => r.resourceType);
-		[Game.rooms[room.name].walls, Game.rooms[room.name].infrastructure] = _.partition(Game.rooms[room.name].damStructures, (s) => s.structureType == STRUCTURE_WALL || s.structureType == STRUCTURE_RAMPART);
-		Game.rooms[room.name].walls = _.sortBy(Game.rooms[room.name].walls, hitsPercentage);
-		Game.rooms[room.name].infrastructure = _.sortBy(Game.rooms[room.name].infrastructure, hitsPercentage);
+
+		//creep variables
+		Game.rooms[room.name].myCreeps = room.find(FIND_MY_CREEPS);
+		Game.rooms[room.name].enemyCreeps = room.find(FIND_HOSTILE_CREEPS);
 	}
 }
 profiler.registerObject(roleRoom, 'roleRoom');
