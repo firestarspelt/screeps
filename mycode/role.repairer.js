5dc5a124cbcb60a23e7874891acdcb294b364f72
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-22 22:09:37 -0400

    Huge Overhaul of repair code

--- mycode/role.repairer.js
+++ mycode/role.repairer.js
@@ -1,82 +1,100 @@
-var roleBuilder = require('role.builder');
-module.exports = {
-	/** @param {Creep} creep **/
-	run: function(creep) {
-		// if creep is trying to repair something but has no energy left
-		if (creep.memory.working && creep.store[RESOURCE_ENERGY] == 0) {
-			creep.memory.working = false;
-			creep.say('🔄 refill');
-			// if creep is full
-		} else if (!creep.memory.working && creep.store[RESOURCE_ENERGY] == creep.store.getCapacity()) {
-			creep.memory.working = true;
-			creep.say('🛠️ repair');
-		}
-		//repair damaged structures within its path while still moving toward target if it has one
-		var structures = creep.room.lookForAtArea(LOOK_STRUCTURES,creep.pos.y - 3,creep.pos.x - 3,creep.pos.y + 3,creep.pos.x + 3, true);
-		for (let structure of structures) {
-			structure = structure['structure'];
-			if (structure.hits < structure.hitsMax - creep.memory.workParts * 100) {
-				creep.repair(structure);
+var roleBuilder = require("role.builder");
+/**
+ * This function gets a low health target and sets the target and targetHealth if it isn't set
+ * targetHealth is only ever set for walls or ramparts
+ * @param  {object <Creep.prototype>} creep the creep getting a target
+ * @param  {number} increment the value to increment by
+ * @param  {array <object <Structure>>} targets an array of target structures
+ */
+function getTarget(creep, increment, targets) {
+	//set intitial percentage to increment value, and keep incrementing while percentage is less than 1
+	for (let percentage = increment; percentage <= 1; percentage = percentage + increment) {
+		//interate through targets
+		for (let target of targets) {
+			//set targetHealth to memory value if it exists, otherwise set it to hitsMax of target
+			if (creep.memory.targetHealth) {
+				let targetHealth = creep.memory.targetHealth;
+			} else {
+				let targetHealth = target.hitsMax;
+			}
+			//if targets hits / targetHealth is less than current percentage set to target
+			if (target.hits / targetHealth < percentage) {
+				//set targetHealth to memory if it wasn't, otherwise set it to itself multiplied by the percentage
+				if (!creep.memory.targetHealth) {
+					creep.memory.targetHealth = targetHealth;
+				} else {
+					creep.memory.targetHealth = creep.memory.targetHealth * percentage;
+				}
+				//set target to memory
+				creep.memory.target = target.id;
+				//break from loop
 				break;
 			}
 		}
+		//if target found break from loop
+		if (creep.memory.target) {
+			break;
+		}
+	}
+}
+module.exports = {
+	/** @param {Creep} creep **/
+	run: function(creep) {
+		//state change based off carried energy
+		switch (creep.store[RESOURCE_ENERGY]) {
+			case creep.store.getCapacity():
+				creep.memory.working = true;
+				creep.say('🛠️ repair');
+				break;
+			case 0:
+				creep.memory.working = false;
+				creep.say('🔄 refill');
+		}
 		//if creep is working
 		if (creep.memory.working) {
-			//get room vars
-			var walls = Game.rooms[creep.room.name].walls;
-			var infrastructure = Game.rooms[creep.room.name].infrastructure;
-			//if repairer doesn't have a target, find one
-			if (!creep.memory.target) {
-				for (let percentage = 0.1; percentage <= 1; percentage = percentage + 0.1) {
-					for (let structure of infrastructure) {
-						if (structure.hits / (structure.hitsMax - creep.memory.workParts * 100) < percentage) {
-							//set target to memory
-							creep.memory.target = structure.id;
-							break;
-						}
-					}
-					if (creep.memory.target) {
-						break;
-					}
+			//repair damaged structures within its path
+			let structures = creep.room.lookForAtArea(LOOK_STRUCTURES,creep.pos.y - 3,creep.pos.x - 3,creep.pos.y + 3,creep.pos.x + 3, true);
+			for (let structure of structures) {
+				let target = structure['structure'];
+				//if it won't over repair target repair it
+				if (target.hits < target.hitsMax - creep.memory.workParts * 100) {
+					creep.repair(target);
+					break;
 				}
+			}
+			//get room vars
+			let walls = Game.rooms[creep.room.name].walls;
+			let infrastructure = Game.rooms[creep.room.name].infrastructure;
+			//if repairer doesn't have a target, find some infrastructure to repair
+			if (!creep.memory.target) {
+				getTarget(creep, 0.1, infrastructure);
+			}
+			//if repairer still doesn't have a target and walls are repairable, find a wall to repair
+			if (!creep.memory.target && creep.room.controller.level > 1) {
+				//set targetHealth for walls and ramparts
+				creep.memory.targetHealth = 3000000;
+				getTarget(creep, 0.01, walls);
 			}
 			//if repairer has target retrieve from memory
 			if (creep.memory.target) {
-				var target = Game.getObjectById(creep.memory.target);
+				let target = Game.getObjectById(creep.memory.target);
 				//if repairer's target would be over repaired purge from memory
-				if (target.hits / (target.hitsMax - creep.memory.workParts * 100) >= 1) {
+				if (target.hits / (creep.memory.targetHealth - creep.memory.workParts * 100) >= 1) {
 					delete creep.memory.target;
+					delete creep.memory.targetHealth;
 				}
+				//if target is still in memory, and in range repair it, if it isn't move into range of it
 				if (creep.memory.target) {
-					//if target is still in memory, and in range repair it, if it isn't move into range of it
 					if (creep.repair(target) == ERR_NOT_IN_RANGE) {
 						creep.travelTo(target,{ignoreCreeps: false, range: 3});
 					}
 				}
-				//if walls are repairable repair up to target health
-			} else if (creep.room.controller.level > 1) {
-				var target;
-				var targetHealth = 3000000;
-				for (let percentage = 0.01; percentage <= 1; percentage = percentage + 0.01) {
-					for (let wall of walls) {
-						if (wall.hits / targetHealth < percentage) {
-							target = wall;
-							break;
-						}
-					}
-					if (target != undefined) {
-						break;
-					}
-				}
-				if (target != undefined) {
-					if (creep.repair(target) == ERR_NOT_IN_RANGE) {
-						creep.travelTo(target,{ignoreCreeps: false, range: 3});
-					}
-				}
-			} else {
+			}//If nothing to repair run builder code
+			else {
 				roleBuilder.run(creep);
 			}
-		} else {
+		}//if not working get energy
+		else {
 			creep.getEnergy();
 		}
 	}
