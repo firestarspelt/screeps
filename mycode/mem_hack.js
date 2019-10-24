f20ab9f942d0a5d27b607746dd8abd532a60ea25
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-24 00:31:27 -0400

    Update mem_hack.js

--- mycode/mem_hack.js
+++ mycode/mem_hack.js
@@ -1,19 +1,21 @@
-global.mem_hack = function(){
-  if(Game.time != global.compile_tick) {
-    if(global.memhack_lastTime && global.memhack_LastMemory && Game.time == (global.memhack_lastTime + 1)){
-      delete global.Memory
-      global.Memory = global.memhack_LastMemory
-      RawMemory._parsed = global.memhack_LastMemory
-    }else{
-      Memory;
-      if(!Game.rooms['sim']) {
-        global.memhack_LastMemory = RawMemory._parsed
-        global.memhack_lastTime = Game.time
-      }
-    }
-  } else {
-    Memory;
-    global.memhack_LastMemory = RawMemory._parsed
-    global.memhack_lastTime = Game.time
-  }
+module.exports = function() {
+	global.mem_hack = function(){
+	  if(Game.time != global.compile_tick) {
+	    if(global.memhack_lastTime && global.memhack_LastMemory && Game.time == (global.memhack_lastTime + 1)){
+	      delete global.Memory
+	      global.Memory = global.memhack_LastMemory
+	      RawMemory._parsed = global.memhack_LastMemory
+	    }else{
+	      Memory;
+	      if(!Game.rooms['sim']) {
+	        global.memhack_LastMemory = RawMemory._parsed
+	        global.memhack_lastTime = Game.time
+	      }
+	    }
+	  } else {
+	    Memory;
+	    global.memhack_LastMemory = RawMemory._parsed
+	    global.memhack_lastTime = Game.time
+	  }
+	}
 }
