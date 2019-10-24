4be635aa8a7e8ee636bfb42c4f6a48601181cfc6
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-24 13:06:44 -0400

    Update role.supplier.js

--- mycode/role.supplier.js
+++ mycode/role.supplier.js
@@ -23,9 +23,9 @@ const roleSupplier = {
 				let targetSpawns = creep.pos.findInRange(targetsByType[STRUCTURE_SPAWN], 15);
 				let targetExtensions = creep.pos.findInRange(targetsByType[STRUCTURE_EXTENSION], 10);
 				let targetTowers = targetsByType[STRUCTURE_TOWER] || [];
-				if (targetSpawns.length > 0 || targetExtensions.length > 0) {
+				if (targetSpawns.length || targetExtensions.length) {
 					var target = creep.pos.findClosestByRange(targetSpawns.concat(targetExtensions));
-				} else if (targetTowers.length > 0) {
+				} else if (targetTowers.length) {
 					var target = creep.pos.findClosestByRange(targetTowers);
 				} else if (creep.room.storage.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
 					var target = creep.room.storage;
