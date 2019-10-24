d0a7321484925589649b2140880bb586f54b411b
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-24 00:11:21 -0400

    Revert "Update prototype.creep.js"
    This reverts commit d44630c89486c5a2603aa3476421fcc7b6c2c696.

--- mycode/prototype.creep.js
+++ mycode/prototype.creep.js
@@ -1,8 +1,4 @@
 module.exports = function() {
-	Creep.prototype.getTarget =
-	function() {
-
-	}
 	Creep.prototype.getEnergy =
 	function() {
 		let resByType = Game.rooms[this.room.name].resByType;
@@ -13,47 +9,45 @@ module.exports = function() {
 		let storage = this.room.storage;
 		let energyStorage = containers.push(storage);
 		let ruins = Game.rooms[this.room.name].ruins;
-		switch (this.memory.role) {
-			case "supplier":
-				if (dropedEnergy.length > 0) {
-					let target = this.pos.findClosestByRange(dropedEnergy);
-					if (this.pickup(target) == ERR_NOT_IN_RANGE) {
-						this.travelTo(target, {ignoreCreeps: false});
-					}
-				}
-				else {
-					let targets = _.filter(containers, (s) => s.store[RESOURCE_ENERGY] >= Math.min(200, this.store.getFreeCapacity(RESOURCE_ENERGY)));
-					if (targets.length > 0) {
-						let target = this.pos.findClosestByRange(targets);
-						if (this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-							this.travelTo(target, {ignoreCreeps: false});
-						}
-					}
-				}
-				break;
-			default:
-				if (dropedEnergy.length) {
-					let target = this.pos.findClosestByRange(dropedEnergy);
-					if (this.pickup(target) == ERR_NOT_IN_RANGE) {
-						this.travelTo(target, {ignoreCreeps: false});
-					}
-				} else if (ruins.length) {
-					var targets = ruins;
-				} else if (!energyStorage.length && this.pos.findClosestByPath(Game.rooms[this.room.name].sources, { ignoreCreeps: false }) !== null) {
-					this.mine();
-				} else if (!energyStorage.length) {
-					var targets = _.filter(spawns, (s) => s.store[RESOURCE_ENERGY] >= Math.min(200, this.store.getFreeCapacity(RESOURCE_ENERGY)));
-				} else if (!this.room.storage) {
-					var targets = _.filter(containers, (s) => s.store[RESOURCE_ENERGY] >= Math.min(200, this.store.getFreeCapacity(RESOURCE_ENERGY)));
-				} else {
-					var target = storage;
-				}
-				if (!target) {
-					var target = this.pos.findClosestByRange(targets);
-				}
-				if (this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+		if (this.memory.role == 'supplier') {
+			if (dropedEnergy.length > 0) {
+				let target = this.pos.findClosestByRange(dropedEnergy);
+				if (this.pickup(target) == ERR_NOT_IN_RANGE) {
 					this.travelTo(target, {ignoreCreeps: false});
 				}
+			}
+			else {
+				let targets = _.filter(containers, (s) => s.store[RESOURCE_ENERGY] >= Math.min(200, this.store.getFreeCapacity(RESOURCE_ENERGY)));
+				if (targets.length > 0) {
+					let target = this.pos.findClosestByRange(targets);
+					if (this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+						this.travelTo(target, {ignoreCreeps: false});
+					}
+				}
+			}
+		} else {
+			if (dropedEnergy.length > 0) {
+				let target = this.pos.findClosestByRange(dropedEnergy);
+				if (this.pickup(target) == ERR_NOT_IN_RANGE) {
+					this.travelTo(target, {ignoreCreeps: false});
+				}
+			} else if (ruins.length > 0) {
+				var targets = ruins;
+			} else if (energyStorage.length == 0 && this.pos.findClosestByPath(Game.rooms[this.room.name].sources, { ignoreCreeps: false }) !== null) {
+				this.mine();
+			} else if (energyStorage.length == 0) {
+				var targets = _.filter(spawns, (s) => s.store[RESOURCE_ENERGY] >= Math.min(200, this.store.getFreeCapacity(RESOURCE_ENERGY)));
+			} else if (!this.room.storage) {
+				var targets = _.filter(containers, (s) => s.store[RESOURCE_ENERGY] >= Math.min(200, this.store.getFreeCapacity(RESOURCE_ENERGY)));
+			} else {
+				var target = storage;
+			}
+			if (!target) {
+				var target = this.pos.findClosestByRange(targets);
+			}
+			if (this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				this.travelTo(target, {ignoreCreeps: false});
+			}
 		}
 	}
 	Creep.prototype.mine =
