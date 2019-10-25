c4b8c5b49ec87355ce9c00b2ec08eb3be4b5acf9
firestarspelt <56907361+firestarspelt@users.noreply.github.com>
2019-10-25 17:58:39 -0600

    Update role.spawner.js

--- mycode/role.spawner.js
+++ mycode/role.spawner.js
@@ -82,7 +82,7 @@ const roleSpawner = {
 				}
 			} else if (spawner.room.memory.claimers < 1) {
 
-				nou big spawn.spawnCreep([ CLAIM , MOVE ], (spawner.name + ' claimer ' + Game.time), {memory: {role: 'claimer', moveParts: 1, totalParts: 2, home: spawner.room.name}});
+				spawner.spawnCreep([ CLAIM , MOVE ], (spawner.name + ' claimer ' + Game.time), {memory: {role: 'claimer', moveParts: 1, totalParts: 2, home: spawner.room.name}});
 
 				if (spawner.spawnCreep == OK ){
 					++spawner.room.memory.claimers;
