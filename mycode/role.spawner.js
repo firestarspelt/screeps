467cb722d263c21fdcd2ededdba6214d0115d4f6
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-25 16:08:11 -0400

    Update role.spawner.js

--- mycode/role.spawner.js
+++ mycode/role.spawner.js
@@ -81,7 +81,7 @@ const roleSpawner = {
 					++spawner.room.memory.builders;
 				}
 			} else if (spawner.room.memory.claimers < 1) {
-				if (spawner.spawnCreep([CLAIM,MOVE], (spawner.name + ' claimer ' + Game.time), {memory: {role: 'claimer', moveParts: 1, totalParts: 2, home: spawner.room.name}}) == OK) {
+				if (spawner.spawnCreep([CLAIM, MOVE], (spawner.name + ' claimer ' + Game.time), {memory: {role: 'claimer', moveParts: 1, totalParts: 2, home: spawner.room.name}}) == OK) {
 					++spawner.room.memory.claimers;
 				}
 			}
