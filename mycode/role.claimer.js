45c1909fa91ac78f37fd8d558b48286170b01919
firestarspelt <56907361+firestarspelt@users.noreply.github.com>
2019-10-25 18:57:37 -0600

    Update role.claimer.js

--- mycode/role.claimer.js
+++ mycode/role.claimer.js
@@ -3,13 +3,14 @@ const roleClaimer = {
 	run: function (creep) {
 		for (let name in Game.flags) {
 			let flag = Game.flags[name];
+		console.log(flag);
 			if (flag.name.includes("Reserve") || flag.name.includes("Claim")) {
 				creep.memory.target = flag.name;
 				break;
 			}
 		}
 		let target = Game.getObjectById(creep.memory.target);
-		console.log(flag);
+		console.log(target);
 		creep.travelTo(target);
 		if (target.name.includes("Reserve")) {
 			creep.reserveController(target);
