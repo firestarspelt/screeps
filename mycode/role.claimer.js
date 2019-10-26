1aca012246fbd6aa36f7d4e1ad2d9e67c9b208f4
firestarspelt <56907361+firestarspelt@users.noreply.github.com>
2019-10-25 18:48:05 -0600

    Update role.claimer.js

--- mycode/role.claimer.js
+++ mycode/role.claimer.js
@@ -9,6 +9,7 @@ const roleClaimer = {
 			}
 		}
 		let target = Game.getObjectById(creep.memory.target);
+		console.log(fuckzuler);
 		creep.travelTo(target);
 		if (target.name.includes("Reserve")) {
 			creep.reserveController(target);
