e5f58022da1e4d8a419198522e774c4d872eac07
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-26 23:17:53 -0400

    Create mem_clear.js

---
+++ mycode/mem_clear.js
@@ -0,0 +1,34 @@
+const profiler = require('screeps-profiler');
+const mem_clear = {
+	run: function () {
+		for (let name in Memory.creeps) {
+			if (!Game.creeps[name]) {
+				let creep = Memory.creeps[name];
+				let room = Game.rooms[creep.home];
+				switch (creep.role) {
+					case "harvester":
+						--room.memory.harvesters;
+						break;
+					case "upgrader":
+						--room.memory.upgraders;
+						break;
+					case "builder":
+						--room.memory.builders;
+						break;
+					case "repairer":
+						--room.memory.repairers;
+						break;
+					case "supplier":
+						--room.memory.suppliers;
+						break;
+					case "claimer":
+						--room.memory.claimers;
+						--Game.flags[creep.memory.target].memory.claimers;
+				}
+				delete Memory.creeps[name];
+			}
+		}
+	}
+}
+profiler.registerObject(mem_clear, 'mem_clear');
+module.exports = mem_clear;
