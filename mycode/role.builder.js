b72b3fa19373684bc31558166e7b8f8bed460179
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-27 20:22:58 -0400

    Update role.builder.js

--- mycode/role.builder.js
+++ mycode/role.builder.js
@@ -17,28 +17,29 @@ const roleBuilder = {
 			if (!creep.memory.target) {
 				creep.getTarget();
 			}
-			//get target from memory
-			let target = Game.getObjectById(creep.memory.target);
-			//if target not valid clear from memory
-			if (!target) {
-				delete creep.memory.target;
-			}
-			//get flag from memory
-			let flag = Game.flags[creep.memory.flag];
-			//if flag is set in memory and no target and there is stuff to build in its room move to it
-			if (flag && !target && flag.room.constuctSites.length && flag.room != creep.room) {
-				creep.travelTo(flag);
-			}
-			//build target
-			else if (creep.build(target) == ERR_NOT_IN_RANGE) {
-				creep.travelTo(target, {ignoreCreeps: false, range: 3});
-			}
-			//if no targets and not in home room move back to homeroom
-			else if (!target && creep.memory.home != creep.room.name) {
-				creep.travelTo(Game.rooms[creep.memory.home]);
-			}
-			//if nothing to build run upgrader code
-			else if (!target) {
+			//get builder has target get from memory
+			if (creep.memory.target) {
+				let target = Game.getObjectById(creep.memory.target);
+				//if target not valid clear from memory
+				if (!target) {
+					delete creep.memory.target;
+				}
+				//get flag from memory
+				let flag = Game.flags[creep.memory.flag];
+				//if flag is set in memory and no target and there is stuff to build in its room move to it
+				if (flag && !target && flag.room.constuctSites.length && flag.room != creep.room) {
+					creep.travelTo(flag);
+				}
+				//build target
+				else if (creep.build(target) == ERR_NOT_IN_RANGE) {
+					creep.travelTo(target, {ignoreCreeps: false, range: 3});
+				}
+				//if no targets and not in home room move back to homeroom
+				else if (!target && creep.memory.home != creep.room.name) {
+					creep.travelTo(Game.rooms[creep.memory.home]);
+				}
+			}//if nothing to build run upgrader code
+			else {
 				roleUpgrader.run(creep);
 			}
 		}//if no energy get some
