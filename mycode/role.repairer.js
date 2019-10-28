e7ba32dd8ce6050be5e063a9ea2593ecc52da455
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-27 20:33:25 -0400

    Update role.repairer.js

--- mycode/role.repairer.js
+++ mycode/role.repairer.js
@@ -42,6 +42,10 @@ const roleRepairer = {
 						creep.travelTo(target,{ignoreCreeps: false, range: 3});
 					}
 				}
+				//if no target and not in home room move back to homeroom
+				else if (!creep.memory.target && creep.memory.home != creep.room.name) {
+					creep.travelTo(Game.rooms[creep.memory.home]);
+				}
 			}//If nothing to repair run upgrader code
 			else {
 				roleUpgrader.run(creep);
