db44b3d2b7e073d363fcaeae58a47f94bec5bc17
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-29 00:12:25 -0400

    Update role.claimer.js

--- mycode/role.claimer.js
+++ mycode/role.claimer.js
@@ -6,14 +6,16 @@ const roleClaimer = {
 		}
 		let flag = Game.flags[creep.memory.flag];
 		creep.travelTo(flag);
-		if (global.reserveFlags.includes(flag)) {
-			if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-				creep.travelTo(creep.room.controller);
+		if (flag && flag.room == creep.room) {
+			if (global.reserveFlags.includes(flag)) {
+				if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+					creep.travelTo(creep.room.controller);
+				}
 			}
-		}
-		else if (global.claimFlags.includes(flag)) {
-			if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-				creep.travelTo(creep.room.controller);
+			else if (global.claimFlags.includes(flag)) {
+				if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+					creep.travelTo(creep.room.controller);
+				}
 			}
 		}
 	}
