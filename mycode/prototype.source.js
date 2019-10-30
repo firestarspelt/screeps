997d05cc805ef8277b071cdf4342e38431598109
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-30 00:54:41 -0400

    Update prototype.source.js

--- mycode/prototype.source.js
+++ mycode/prototype.source.js
@@ -3,23 +3,23 @@ module.exports = function() {
 	Object.defineProperty(Source.prototype, 'memory', {
 		configurable: true,
 		get: function() {
-			if(_.isUndefined(Memory.rooms[this.id].sources)) {
-				Memory.rooms[this.id].sources = {};
+			if(_.isUndefined(Memory.rooms[this.room].sources)) {
+				Memory.rooms[this.room].sources = {};
 			}
-			if(!_.isObject(Memory.rooms[this.id].sources[this.id])) {
+			if(!_.isObject(Memory.rooms[this.room].sources[this.id])) {
 				return undefined;
 			}
-			return Memory.rooms[this.id].sources[this.id] =
-					Memory.rooms[this.id].sources[this.id] || {};
+			return Memory.rooms[this.room].sources[this.id] =
+					Memory.rooms[this.room].sources[this.id] || {};
 		},
 		set: function(value) {
-			if(_.isUndefined(Memory.rooms[this.id].sources)) {
-				Memory.rooms[this.id].sources = {};
+			if(_.isUndefined(Memory.rooms[this.room].sources)) {
+				Memory.rooms[this.room].sources = {};
 			}
-			if(!_.isObject(Memory.rooms[this.id].sources)) {
+			if(!_.isObject(Memory.rooms[this.room].sources)) {
 				throw new Error('Could not set source memory');
 			}
-			Memory.rooms[this.id].sources[this.id] = value;
+			Memory.rooms[this.room].sources[this.id] = value;
 		}
 	});
 
