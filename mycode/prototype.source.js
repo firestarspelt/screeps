693661b53179cb06006980cbd4f4d5dfc3fb0476
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-30 00:50:38 -0400

    Update prototype.source.js

--- mycode/prototype.source.js
+++ mycode/prototype.source.js
@@ -3,23 +3,23 @@ module.exports = function() {
 	Object.defineProperty(Source.prototype, 'memory', {
 		configurable: true,
 		get: function() {
-			if(_.isUndefined(Memory.mySourcesMemory)) {
-				Memory.mySourcesMemory = {};
+			if(_.isUndefined(this.room.memory.sources)) {
+				this.room.memory.sources = {};
 			}
-			if(!_.isObject(Memory.mySourcesMemory)) {
+			if(!_.isObject(this.room.memory.sources)) {
 				return undefined;
 			}
-			return Memory.mySourcesMemory[this.id] =
-					Memory.mySourcesMemory[this.id] || {};
+			return this.room.memory.sources[this.id] =
+					this.room.memory.sources[this.id] || {};
 		},
 		set: function(value) {
-			if(_.isUndefined(Memory.mySourcesMemory)) {
-				Memory.mySourcesMemory = {};
+			if(_.isUndefined(this.room.memory.sources)) {
+				this.room.memory.sources = {};
 			}
-			if(!_.isObject(Memory.mySourcesMemory)) {
+			if(!_.isObject(this.room.memory.sources)) {
 				throw new Error('Could not set source memory');
 			}
-			Memory.mySourcesMemory[this.id] = value;
+			this.room.memory.sources[this.id] = value;
 		}
 	});
 
