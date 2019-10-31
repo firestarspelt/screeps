a085b6d073c3fb32d25cc4dbd09ad276f1b1b24d
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-31 17:02:32 -0400

    Update prototype.creep.js

--- mycode/prototype.creep.js
+++ mycode/prototype.creep.js
@@ -45,7 +45,7 @@ module.exports = function() {
 						let maintain = global.maintainFlags;
 						for (let flag of maintain) {
 							switch (flag.memory.repairers) {
-								case false: {
+								default: {
 									this.memory.flag = flag.name;
 									flag.memory.repairers = 1;
 									break flagTarget;
@@ -81,7 +81,7 @@ module.exports = function() {
 					let claim = global.claimFlags;
 					for (let flag of claim) {
 						switch (flag.memory.claimers) {
-							case false: {
+							default: {
 								this.memory.flag = flag.name;
 								flag.memory.claimers = 1;
 								break flagTarget;
@@ -96,7 +96,7 @@ module.exports = function() {
 					let reserve = global.reserveFlags;
 					for (let flag of reserve) {
 						switch (flag.memory.claimers) {
-							case false: {
+							default: {
 								this.memory.flag = flag.name;
 								flag.memory.claimers = 1;
 								break flagTarget;
@@ -139,7 +139,7 @@ module.exports = function() {
 					let maintain = global.maintainFlags;
 					for (let flag of maintain) {
 						switch (flag.memory.builders) {
-							case false: {
+							default: {
 								this.memory.flag = flag.name;
 								flag.memory.builders = 1;
 								break flagTarget;
@@ -187,7 +187,7 @@ module.exports = function() {
 					sourceTarget: {
 						for (let source of this.room.sources) {
 							switch (source.memory.suppliers) {
-								case false: {
+								default: {
 									this.memory.source = source.id;
 									source.memory.suppliers = 1;
 									break sourceTarget;
