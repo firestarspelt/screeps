6057ddd636758d77dfd24f94e1e8faf2d55045cf
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-31 16:38:52 -0400

    Update prototype.creep.js

--- mycode/prototype.creep.js
+++ mycode/prototype.creep.js
@@ -208,7 +208,7 @@ module.exports = function() {
 					if (this.pickup(energySupply, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 						this.travelTo(energySupply, {ignoreCreeps: false, offRoad: true});
 					}
-					if (this.store.getFreeCapacity() > 0) {
+					if (energySupply && this.store.getFreeCapacity() > 0) {
 						let amount = this.store.getFreeCapacity() - energySupply.store[RESOURCE_ENERGY];
 						energySupply = this.pos.findClosestByRange(containers);
 						this.withdraw(energySupply, RESOURCE_ENERGY, amount);
