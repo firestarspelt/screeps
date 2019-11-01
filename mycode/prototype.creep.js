bada36b662656a4c7954a5e402626a224ca2190a
Zuler0 <44147781+Zuler0@users.noreply.github.com>
2019-10-31 20:28:09 -0400

    Update prototype.creep.js

--- mycode/prototype.creep.js
+++ mycode/prototype.creep.js
@@ -267,6 +267,9 @@ module.exports = function() {
 				else if (spawns) {
 					energySupplies = _.filter(spawns, (s) => s.store[RESOURCE_ENERGY] >= Math.min(200, this.store.getFreeCapacity(RESOURCE_ENERGY)));
 				}
+				else {
+					this.travelTo(Game.rooms[creep.memory.home].controller);
+				}
 				if (energySupplies) {
 					energySupply = this.pos.findClosestByRange(energySupplies);
 				}
